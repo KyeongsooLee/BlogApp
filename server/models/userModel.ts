ffd35fd8f400c10f7ceeac7f1e565a1b14977865
KyeongsooLee <leeksca26@gmail.com>
2023-01-04 15:19:36 -0500

    20230104 01

---
+++ server/models/userModel.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add your name"],
+        trim: true,
+        maxLength: [20, "Your name is up to 20 chars long."]
+    },
+    account: {
+        type: String,
+        required: [true, "Please add your email or phone"],
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please add your password"],
+        trim: true,
+        unique: true
+    },
+    avatar: {
+        type: String,
+        default: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752405/avatar/avatar_cugq40.png'
+    },
+    type: {
+        type: String,
+        default: 'normal' // fast
+    }
+}, {
+    timestamps: true
+})
+
+export default mongoose.model('User', userSchema)
