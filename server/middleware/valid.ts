378b5008c334788130965091f465d82172aaf7ad
KyeongsooLee <leeksca26@gmail.com>
2023-01-06 13:43:28 -0500

    20230106 01

--- server/middleware/valid.ts
+++ server/middleware/valid.ts
@@ -1,35 +1,39 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const validRegister = async (req: Request, res: Response, next: NextFunction) => {
-    const { name, account, password } = req.body
-    
-    if(!name){
-        return res.status(400).json({msg: "please add your name."})
-    }else if(name.length > 20){
-        return res.status(400).json({msg: "Your name is up to 20 chars long."})
-    }
+  const { name, account, password } = req.body
 
-    if(!account){
-        return res.status(400).json({msg: "please add your email or phone number."})
-    }else if(!validPhone(account) && !validateEmail(account)){
-        return res.status(400).json({msg: "Email or phone number format is incorrect."})
-    }
+  const errors = [];
 
-    if(password.length < 6) {
-        return res.status(400).json({msg: "Password must be at least 6 chars."})
-    }
+  if(!name){
+    errors.push("Please add your name.")
+  }else if(name.length > 20){
+    errors.push("Your name is up to 20 chars long.")
+  }
 
-    next();
+  if(!account){
+    errors.push("Please add your email or phone number.")
+  }else if(!validPhone(account) && !validateEmail(account)){
+    errors.push("Email or phone number format is incorrect.")
+  }
+
+  if(password.length < 6){
+    errors.push("Password must be at least 6 chars.")
+  }
+
+  if(errors.length > 0) return res.status(400).json({msg: errors})
+
+  next();
 }
 
 
 
-function validPhone(phone: string) {
-    const re = (/^[+]/g)
-    return re.test(phone)
+export function validPhone(phone: string) {
+  const re = /^[+]/g
+  return re.test(phone)
 }
 
-function validateEmail(email: string){
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return re.test(String(email).toLowerCase());
+export function validateEmail(email: string) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
