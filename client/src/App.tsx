79bbda54aa5ca2ac783adbd8ef139587576515de
KyeongsooLee <leeksca26@gmail.com>
2023-01-09 14:19:06 -0500

    20230109 01

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' // Routes instead of Switch
+
+import PageRender from './PageRender'
+
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PageRender />} />
+          <Route path="/:page" element={<PageRender />} />
+          <Route path="/:page/:slug" element={<PageRender />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
