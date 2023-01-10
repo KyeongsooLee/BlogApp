acca9745bf53b21b53afc898edbb2b34254326a7
KyeongsooLee <leeksca26@gmail.com>
2023-01-10 14:26:19 -0500

    20230110 Redux Header Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' // Routes instead of Switch
 
 import PageRender from './PageRender'
+import Header from './components/global/Header'
+import Footer from './components/global/Footer'
 
 
 const App = () => {
   return (
     <div className='container'>
       <Router>
+        <Header />
+
         <Routes>
           <Route path="/" element={<PageRender />} />
           <Route path="/:page" element={<PageRender />} />
           <Route path="/:page/:slug" element={<PageRender />} />
         </Routes>
+
+        <Footer/>
       </Router>
     </div>
   )
---
+++ client/src/components/global/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      <h6>Welcome to my chanel DevLee</h6>
+      <a href="https://www.linkedin.com/in/kyeongsoo-lee-012796203/" 
+      target="_blank" rel="noreferrer"
+      className="mb-2 d-block">
+        https://www.linkedin.com/in/kyeongsoo-lee-012796203/
+      </a>
+      <p> Copyright &copy; 2023</p>
+    </div>
+  )
+}
+
+export default Footer
