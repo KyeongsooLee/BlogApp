c9a9c620cbf5a8706ccb4b909f722dd285d8f8b6
KyeongsooLee <leeksca26@gmail.com>
2023-01-30 10:19:42 -0500

    20230130 Refresh Token & Logout - 5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' // Routes instead of Switch
+import { useDispatch } from 'react-redux'
 
 import PageRender from './PageRender'
 import Header from './components/global/Header'
 import Footer from './components/global/Footer'
 
 import { Alert } from './components/alert/Alert'
+import { refreshToken } from './redux/actions/authAction'
 
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(refreshToken())
+  },[dispatch])
+
   return (
     <div className='container'>
       <Router>
