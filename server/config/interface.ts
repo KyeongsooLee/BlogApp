fa75db2fe4e51d6fb315d8d9aa080c8e9e9d749c
KyeongsooLee <leeksca26@gmail.com>
2023-02-06 12:41:20 -0500

    20230206 Google Login - 5

--- server/config/interface.ts
+++ server/config/interface.ts
@@ -1,4 +1,4 @@
-import { Document, StringSchemaDefinition } from 'mongoose'
+import { Document } from 'mongoose'
 
 export interface IUser extends Document{
     name: String
@@ -23,3 +23,18 @@ export interface IDecodedToken {
     iat: number
     exp: number
 }
+
+export interface IGgPayload {
+    email: string
+    email_verified: boolean
+    name: string
+    picture: string
+  }
+  
+  export interface IUserParams {
+    name: string 
+    account: string 
+    password: string
+    avatar?: string
+    type: string
+  }
